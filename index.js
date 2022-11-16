e668bcb0de5ccc78b8653520903e466ba508622e
juancristobaldev <juancristobaldeveloper@gmail.com>
2022-11-16 17:28:20 -0300

    vercel

--- index.js
+++ index.js
@@ -1,17 +1,77 @@
-// Add Express
-const express = require("express");
+//packages
 
-// Initialize Express
-const app = express();
 
-// Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+const {buildSchema} = require('graphql')
+const {graphqlHTTP} = require('express-graphql')
 
-// Initialize server
-app.listen(3000, () => {
-  console.log("Running on port 3000.");
-});
+const {readFileSync} = require('fs')
+const {join} = require('path')
 
-module.exports = app;
+const express = require('express'),
+app = express()
+
+const cors = require('cors'),
+bodyparser = require('body-parser'),
+session = require('express-session'),
+cookieParser = require('cookie-parser')
+
+//env
+
+require('dotenv').config({path:'./.env'})
+
+//middlewares
+app.use(cookieParser())
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+app.use(cors());
+
+app.use(session({
+    secret: 'mysecretkey',
+    resave:true,
+    saveUninitialized:true,
+    cookie: 
+    { 
+        path: '/',
+        httpOnly: true,
+        secure: false,
+        maxAge: 7200000,
+    }
+}));
+
+const resolvers = require('./lib/resolvers')
+
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    )
+)
+
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue:resolvers,
+    graphiql:true,
+
+}))
+
+// port
+
+const port = process.env.PORT;
+
+// routes
+
+app.get( '/', (req,res) => {
+    res.send('Api funcionando')
+})
+
+app.get('/cookie' ,(req,res) => {
+    res.send('done')
+})
+
+
+app.listen(port,() => {
+    console.log('Corriendo puerto en puerto ' + port)
+})
+
